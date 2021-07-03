11a6768f856457a72b2c99d8904b253c0ab609ed
Jupegarnica <juan@geekshubs.com>
2021-07-03 12:00:59 +0200

    working

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { delay as wait } from "https://deno.land/std@0.100.0/async/mod.ts";
+
+addEventListener("fetch", async (event) => {
+  console.log(event.request.method, event.request.url);
+
+  const { pathname, searchParams } = new URL(event.request.url);
+  const body = searchParams.get("body");
+  const _headers = searchParams.get("headers");
+  const headers = _headers ? JSON.parse(_headers) : {
+    "content-type": "text/plain",
+  };
+  const status = searchParams.get("status");
+  const _delay = searchParams.get("delay");
+  const delay = _delay ? Number(_delay) : 0;
+
+  if (delay) {
+    await wait(delay);
+  }
+  event.respondWith(
+    new Response(body, {
+      status: status ? Number(status) : 200,
+      headers: headers,
+    }),
+  );
+});
---
+++ test.ts
@@ -0,0 +1,71 @@
+import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
+
+Deno.test({
+  name: "default response to status 200 and empty text body",
+  fn: async () => {
+    const response = await fetch("http://localhost:8080");
+    const body = await response.text();
+    assertEquals(response.status, 200);
+    assertEquals(body, "");
+    assertEquals(response.headers.get("content-type"), "text/plain");
+  },
+});
+
+Deno.test({
+  name: "should respond with status specified",
+  fn: async () => {
+    const response = await fetch("http://localhost:8080?status=201");
+    const body = await response.text();
+    assertEquals(response.status, 201);
+    assertEquals(body, "");
+    assertEquals(response.headers.get("content-type"), "text/plain");
+  },
+});
+Deno.test({
+  name: "should respond with headers specified",
+  fn: async () => {
+    const response = await fetch("http://localhost:8080?body=hello%20world");
+    const body = await response.text();
+    assertEquals(response.status, 200);
+    assertEquals(body, "hello world");
+    assertEquals(response.headers.get("content-type"), "text/plain");
+  },
+});
+Deno.test({
+  name: "should respond with body specified",
+  fn: async () => {
+    const response = await fetch(
+      `http://localhost:8080?headers={"x-hello":"world"}`,
+    );
+    const body = await response.text();
+    assertEquals(response.headers.get("x-hello"), "world");
+  },
+});
+
+Deno.test({
+  name: "should respond with json body",
+  fn: async () => {
+    const response = await fetch(
+      `http://localhost:8080?body={"a":1}&headers={"content-type":"application/json"}`,
+    );
+    const body = await response.json();
+    assertEquals(response.status, 200);
+    assertEquals(body, { a: 1 });
+    assertEquals(response.headers.get("content-type"), "application/json");
+  },
+});
+
+Deno.test({
+  name: "response should be delayed at least the delay",
+  fn: async () => {
+    const delay = 100;
+    const start = Date.now();
+    const response = await fetch(
+      `http://localhost:8080?delay=${delay}`,
+    );
+    const duration = Date.now() - start;
+    const body = await response.text();
+    assertEquals(response.status, 200);
+    assertEquals(duration > delay, true);
+  },
+});
