38ce7c5c6447269a0c68416ceea5713799a499e6
Jupegarnica <juan@geekshubs.com>
2021-07-04 17:59:46 +0200

    fix handeling error

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 import { delay as wait } from "https://deno.land/std@0.100.0/async/mod.ts";
 
 addEventListener("fetch", async (event) => {
+  console.log(event.request.method, event.request.url);
+
+  const { pathname, searchParams } = new URL(event.request.url);
+  const body = searchParams.get("body");
+  const _headers = searchParams.get("headers");
+  const headers = _headers ? JSON.parse(_headers) : undefined;
+  const status = searchParams.get("status");
+  const _delay = searchParams.get("delay");
+  const delay = _delay ? Number(_delay) : 0;
+  const responseHeaders = { "Access-Control-Allow-Origin": "*", ...headers };
   try {
-    console.log(event.request.method, event.request.url);
-
-    const { pathname, searchParams } = new URL(event.request.url);
-    const body = searchParams.get("body");
-    const _headers = searchParams.get("headers");
-    const headers = _headers ? JSON.parse(_headers) : undefined;
-    const status = searchParams.get("status");
-    const _delay = searchParams.get("delay");
-    const delay = _delay ? Number(_delay) : 0;
-
     if (delay) {
       await wait(delay);
     }
@@ -20,7 +20,6 @@ addEventListener("fetch", async (event) => {
       const readme = new URL("readme.html", import.meta.url);
       return event.respondWith(fetch(readme));
     }
-    const responseHeaders = { "Access-Control-Allow-Origin": "*", ...headers };
     event.respondWith(
       new Response(body, {
         status: status ? Number(status) : 200,
